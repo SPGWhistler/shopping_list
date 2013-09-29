813193e5d529a3322f59c052154080fb384042aa
Tony Petty <tpetty@appmobi.com>
2013-09-28 21:44:59 -0400

    Added simple cache file.

--- list.js
+++ list.js
@@ -1,4 +1,5 @@
 /*global phantom */
+var fs = require('fs');
 var aldi = require('./aldi_weeklyad');
 var tops = require('./tops_weeklyad');
 var target_weekly = require('./target_weeklyad');
@@ -11,27 +12,46 @@ var ads = [
 ];
 var products = [];
 var i;
+var cache_file = 'cache.json';
 
-for (i in ads) {
-	if (ads.hasOwnProperty(i)) {
-		ads[i].getAds();
+var handleResults = function () {
+	var j;
+	console.log('total products: ' + products.length);
+	for (j in products) {
+		if (products.hasOwnProperty(j)) {
+			console.log(products[j].description);
+		}
 	}
-}
+	phantom.exit();
+};
 
-setInterval(function () {
-	var i;
-	if (ads.length) {
-		for (i in ads) {
-			if (ads.hasOwnProperty(i)) {
-				if (ads[i].products.length) {
-					console.log(ads[i].products.length);
-					products = products.concat(ads[i].products);
-					ads.splice(i, 1);
+if (fs.exists(cache_file)) {
+	//Cache file exists, and we are not forcing a refresh
+	products = JSON.parse(fs.read(cache_file));
+	handleResults();
+} else {
+	//Cache file does not exist, or we are forced to do a refresh
+	for (i in ads) {
+		if (ads.hasOwnProperty(i)) {
+			ads[i].getAds();
+		}
+	}
+
+	setInterval(function () {
+		var i;
+		if (ads.length) {
+			for (i in ads) {
+				if (ads.hasOwnProperty(i)) {
+					if (ads[i].products.length) {
+						console.log(ads[i].products.length);
+						products = products.concat(ads[i].products);
+						ads.splice(i, 1);
+					}
 				}
 			}
+		} else {
+			fs.write(cache_file, JSON.stringify(products), 'w');
+			handleResults();
 		}
-	} else {
-		console.log('total products: ' + products.length);
-		phantom.exit();
-	}
-}, 500);
+	}, 500);
+}
