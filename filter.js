6adec2178406e43a254b056dda34194a7c66ca02
Tony Petty <tpetty@appmobi.com>
2013-09-28 23:12:20 -0400

    added a filter js.

---
+++ filter.js
@@ -0,0 +1,19 @@
+/*global phantom, $ */
+var patterns = [
+	"^(?=.*apples )((?!candy).)*$",
+	"^.*strawberries.*$"
+];
+exports.filter = function (list) {
+	var newList = [];
+	list.forEach(function (arg) {
+		patterns.some(function (pat) {
+			var re = new RegExp(pat, "gmi");
+			if (arg.description.search(re) !== -1) {
+				newList.push(arg);
+				return true;
+			}
+			return false;
+		});
+	});
+	return newList;
+};
--- list.js
+++ list.js
@@ -1,5 +1,7 @@
 /*global phantom */
+var args = require('system').args;
 var fs = require('fs');
+var filter = require('./filter').filter;
 var aldi = require('./aldi_weeklyad');
 var tops = require('./tops_weeklyad');
 var target_weekly = require('./target_weeklyad');
@@ -13,19 +15,46 @@ var ads = [
 var products = [];
 var i;
 var cache_file = 'cache.json';
+var options = {
+	force_refresh: false
+};
+
+//Parse arguments
+if (args.length > 1) {
+	args.forEach(function (arg) {
+		switch (arg) {
+		case "-f":
+			options.force_refresh = true;
+			break;
+		case "-h":
+		case "?":
+		case "-?":
+		case "--help":
+			console.log('Available Options:');
+			console.log('-f    Force a cache refresh');
+			phantom.exit();
+			break;
+		}
+	});
+}
 
 var handleResults = function () {
-	var j;
-	console.log('total products: ' + products.length);
-	for (j in products) {
-		if (products.hasOwnProperty(j)) {
-			console.log(products[j].description);
+	var j, newProducts = [];
+	newProducts = filter(products);
+	for (j in newProducts) {
+		if (newProducts.hasOwnProperty(j)) {
+			console.log(newProducts[j].description);
+			console.log('     ' + newProducts[j].price);
+			console.log('     ' + newProducts[j].link);
+			console.log('     ' + newProducts[j].image);
 		}
 	}
+	console.log('total products: ' + products.length);
+	console.log('filtered products: ' + newProducts.length);
 	phantom.exit();
 };
 
-if (fs.exists(cache_file)) {
+if (fs.exists(cache_file) && options.force_refresh === false) {
 	//Cache file exists, and we are not forcing a refresh
 	products = JSON.parse(fs.read(cache_file));
 	handleResults();
